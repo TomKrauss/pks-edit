4f32f984d6436c82b692a95620cfe455db9cf71c
Tom Krauss <tom.krauss@gebit.de>
2024-04-28 07:58:52 +0200

    #12: implemented Multi-select opening of files with IFileDialog API.

--- pkslib/OpenFileDialog.cpp
+++ pkslib/OpenFileDialog.cpp
@@ -17,6 +17,7 @@
 #include "pch.h"
 #include <objidl.h>
 #include <ShObjIdl.h>
+#include <Shlwapi.h>
 #include "..\include\fileselector.h"
 
 static void file_open_free_filters(COMDLG_FILTERSPEC* pFilters, int nCount) {
@@ -60,6 +61,69 @@ static COMDLG_FILTERSPEC* file_open_build_filters(char* pszFileTypes, char* pszC
     return pSpec;
 }
 
+/*
+ * Return the multi-selection result from a file dialog. In the case of multi-selection the result is returned in
+ * the fillowing format:
+ * dirname\0baseFileName1\0baseFileName2...\0\0
+ */
+static int file_getResults(IFileOpenDialog* pFileOpenDialog, char* pszResult) {
+    IShellItemArray* psiaResults;
+    HRESULT hr = pFileOpenDialog->GetResults(&psiaResults);
+    if (!SUCCEEDED(hr)) {
+        return 0;
+    }
+    char fullPath[MAX_PATH];
+    char fileName[MAX_PATH];
+    DWORD nCount;
+    char* pEnd = pszResult + 4095;
+    int bFirst = 1;
+    psiaResults->GetCount(&nCount);
+    for (DWORD i = 0; i < nCount; i++) {
+        IShellItem* pItem;
+        psiaResults->GetItemAt(i, &pItem);
+        PWSTR pszFilePath;
+        pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+        WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, fullPath, sizeof(fullPath), 0, 0);
+        strcpy_s(fileName, MAX_PATH, fullPath);
+        PathStripPathA(fileName);
+        if (bFirst) {
+            fullPath[strlen(fullPath) - 1 - strlen(fileName)] = 0;
+            strcpy_s(pszResult, MAX_PATH, fullPath);
+            pszResult += strlen(pszResult) + 1;
+            bFirst = 0;
+        }
+        strcpy_s(pszResult, MAX_PATH, fileName);
+        pszResult += strlen(pszResult) + 1;
+        *pszResult = 0;
+        CoTaskMemFree(pszFilePath);
+    }
+    psiaResults->Release();
+    return 1;
+}
+
+/*
+ * Return the single selected result from a file dialog.
+ */
+static int file_getResult(IFileDialog* pFileDialog, char* pszResult) {
+    IShellItem* pItem;
+    HRESULT hr = pFileDialog->GetResult(&pItem);
+    if (!SUCCEEDED(hr)) {
+        return 0;
+    }
+    int ret = 0;
+    PWSTR pszFilePath;
+    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+    // Display the file name to the user.
+    if (SUCCEEDED(hr)) {
+        ret = 1;
+        WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, pszResult, 1024, 0, 0);
+        CoTaskMemFree(pszFilePath);
+        ret = 1;
+    }
+    pItem->Release();
+    return ret;
+}
+
 extern "C" __declspec(dllexport) int file_open_vista_version(FILE_SELECT_PARAMS* pParams) {
     int ret = 0;
     HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
@@ -81,38 +145,25 @@ extern "C" __declspec(dllexport) int file_open_vista_version(FILE_SELECT_PARAMS*
             MultiByteToWideChar(CP_UTF8, 0, pParams->fsp_title, -1, wTitle, sizeof wTitle / 4);
             pFileDialog->SetTitle(wTitle);
             DWORD dwOptions;
-            // Specify multiselect.
+            // In case of multi-select - set options.
             pFileDialog->GetOptions(&dwOptions);
-            if (pParams->fsp_multiSelect) {
-                //dwOptions |= FOS_ALLOWMULTISELECT;
-                //pfd->SetOptions(dwOptions);
+            if (pParams->fsp_multiSelect && !pParams->fsp_saveAs) {
+                dwOptions |= FOS_ALLOWMULTISELECT;
+                pFileDialog->SetOptions(dwOptions);
             }
             hr = pFileDialog->Show(NULL);
-
             // Get the file name from the dialog box.
             if (SUCCEEDED(hr)) {
-                IShellItem* pItem;
-                hr = pFileDialog->GetResult(&pItem);
-                if (SUCCEEDED(hr)) {
-                    if (pParams->fsp_multiSelect && !pParams->fsp_saveAs) {
-                        // Obtain the result of the user interaction.
-                        IFileOpenDialog* pFileOpenDialog = (IFileOpenDialog*)pFileDialog;
-                        IShellItemArray* psiaResults;
-                        hr = pFileOpenDialog->GetResults(&psiaResults);
-                        psiaResults->Release();
-                    }
-                    PWSTR pszFilePath;
-                    hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-                    // Display the file name to the user.
-                    if (SUCCEEDED(hr)) {
-                        ret = 1;
-                        WideCharToMultiByte(CP_UTF8, 0, pszFilePath, -1, pParams->fsp_resultFile, 1024, 0, 0);
-                        CoTaskMemFree(pszFilePath);
-                        pFileDialog->GetFileTypeIndex(&nSelected);
-                        WideCharToMultiByte(CP_UTF8, 0, pFileTypes[nSelected].pszSpec, -1, pParams->fsp_namePatterns, 1024, 0, 0);
-                    }
-                    pItem->Release();
+                if (pParams->fsp_multiSelect && !pParams->fsp_saveAs) {
+                    // Obtain the result of the user interaction.
+                    IFileOpenDialog* pFileOpenDialog = (IFileOpenDialog*)pFileDialog;
+                    file_getResults(pFileOpenDialog, pParams->fsp_resultFile);
+                } else {
+                    file_getResult(pFileDialog, pParams->fsp_resultFile);
                 }
+                pFileDialog->GetFileTypeIndex(&nSelected);
+                WideCharToMultiByte(CP_UTF8, 0, pFileTypes[nSelected].pszSpec, -1, pParams->fsp_namePatterns, 1024, 0, 0);
+                ret = 1;
             }
             file_open_free_filters(pFileTypes, count);
             pFileDialog->Release();
