31f20d55117815db040b9e8f9efe8cedfb11c649
Tom Krauss <tom.krauss@gebit.de>
2024-08-06 13:08:19 +0200

    NO-ISSUE: added PHP grammar.

---
+++ examples/sample.php
@@ -0,0 +1,19 @@
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+
+print "<h2>" . $txt1 . "</h2>";
+print "<p>Study PHP at " . $txt2 . "</p>";
+
+function myMessage() {
+  echo "Hello world!";
+}
+
+myMessage();
+
